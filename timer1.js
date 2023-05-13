d682cc2234b5b51dd49dbd0fe7a70ccbc536a9bb
iDebonair <ofemiowolabi@gmail.com>
2023-05-13 22:53:45 +0000

    Modified code to accept input from the user

--- timer1.js
+++ timer1.js
@@ -1,12 +1,17 @@
 const alarm = (time) =>{
-  if(time === Number(time) && time >= 0){
+  if (time === Number(time) && time >= 0) {
     setTimeout(() => {
       process.stdout.write('\x07');
-    },time * 1000);
-  }else{
-    process.stdout.write('NaN')
+    }, time * 1000);
+  } else {
+    process.stdout.write('NaN');
   }
 };
-alarm(10);
-alarm(3);
-alarm(5);
+
+// Get the command line arguments
+const args = process.argv.slice(2);
+
+// Call the alarm function with each argument
+for (let i = 0; i < args.length; i++) {
+  alarm(parseInt(args[i]));
+}
