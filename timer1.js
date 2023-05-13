9b0650028b1ed991762dd7253f3efeb394409e53
iDebonair <ofemiowolabi@gmail.com>
2023-05-13 22:54:45 +0000

    Modified code to accept input from the user

--- timer1.js
+++ timer1.js
@@ -8,10 +8,10 @@ const alarm = (time) =>{
   }
 };
 
-// Get the command line arguments
+// Accepts input from the terminal
 const args = process.argv.slice(2);
 
-// Call the alarm function with each argument
+// Call the alarm function and the argument recieved from the user
 for (let i = 0; i < args.length; i++) {
   alarm(parseInt(args[i]));
 }
